12eb6985c90c6b673fe7853695fd3838c06ea95f
Peafowll <tudorpaun16@gmail.com>
2025-11-11 02:39:00 +0200

    Add champion data and helper module
    Added championFull.json containing detailed champion data. Introduced helpers.py for utility functions. Updated cogs/games.py to utilize new data and helpers.

--- cogs/games.py
+++ cogs/games.py
@@ -1,13 +1,16 @@
 import discord
 from discord.ext import commands
-import requests
+from helpers import find_lol_spells
+import random
+champion_ults = find_lol_spells()
+
 class Games(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
     
     @commands.command()
     async def ult_game(self,ctx):
-        """Lets you play the League of Legends ultimate name game."""
+        """Plays a game of the LoL Ult Name guessing game."""
         author = ctx.author
         await ctx.send("Let's Play!")
 
---
+++ helpers.py
@@ -0,0 +1,21 @@
+import requests
+import json
+import os
+def find_lol_spells():
+    with open("championFull.json", "r", encoding="utf8") as file:
+        data=json.load(file)
+    abilties = {}
+    for champion_name,champion_data in data["data"].items():
+        q_name,w_name,e_name,r_name = [champion_data["spells"][n]["name"]  for n in range(4)]
+        passive_name = champion_data["passive"]["name"]
+        abilties[champion_name] = [passive_name,q_name,w_name,e_name,r_name]
+    return abilties
+
+def resfresh_ult_json():
+    response = requests.get("https://ddragon.leagueoflegends.com/cdn/15.21.1/data/en_US/championFull.json")
+    data=response.text
+    with open("championFull.json", "w", encoding="utf8") as file:
+        file.write(response.text)
+
+
+find_lol_spells()
